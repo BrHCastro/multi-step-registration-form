2f1dfd7563b950a724f19829412f3fa0e78dd19b
BrHCastro <hendecastro@gmail.com>
2022-06-13 00:03:30 -0300

    fixed bug temp

--- src/pages/FormStep2/index.tsx
+++ src/pages/FormStep2/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import * as C from "./styles";
 
 import { useEffect } from "react";
--- src/pages/FormStep3/index.tsx
+++ src/pages/FormStep3/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import * as C from "./styles";
 
 import { ChangeEvent, useEffect } from "react";
